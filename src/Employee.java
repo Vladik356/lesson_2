e0ccba77408c602628e7fe2160caaa05261cff29
Vladislav <44534111+Vladik356@users.noreply.github.com>
2024-08-11 23:17:19 +0300

    Initial commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phone;
+    private float salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phone, float salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("ФИО: " + name);
+        System.out.println("Должность: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Телефон: " + phone);
+        System.out.println("Зарплата: " + salary);
+        System.out.println("Возраст: " + age);
+    }
+
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[5];
+        employees[0] = new Employee("Сергей Лазарев", "Электрик", "lazarev@gmail.com", "89993289999", 40000, 48);
+        employees[1] = new Employee("Эван Питорс", "Актер", "pitors@gmail.com", "89993289998", 100000, 37);
+        employees[2] = new Employee("Михал Зубенко", "Прараб", "Zybenko@yandex.com", "89993289997", 500, 35);
+        employees[3] = new Employee("Андрей Голубев", "Стример", "Golybev@gmail.com", "89993289996", 200000, 39);
+        employees[4] = new Employee("Алексей Березин", "Киберспортсмен", "Berezin@rambler.com", "89993289995", 5000, 39);
+
+        for (Employee employee : employees) {
+            employee.printInfo();
+            System.out.println();
+        }
+    }
 }
--- src/Park.java
+++ src/Park.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class Park {
+import java.util.ArrayList;
+
+public class Park {
+    private ArrayList<Attraction> attractions;
+
+    public Park() {
+        this.attractions = new ArrayList<>();
+    }
+
+    public void addAttraction(String name, String workingHours, float price) {
+        this.attractions.add(new Attraction(name, workingHours, price));
+    }
+
+    public void allAttractions() {
+        for (Attraction attraction : attractions) {
+            System.out.println("Название: " + attraction.name);
+            System.out.println("Время работы: " + attraction.workingHours);
+            System.out.println("Стоимость: " + attraction.price);
+            System.out.println();
+        }
+    }
+
+    private class Attraction {
+        private String name;
+        private String workingHours;
+        private float price;
+
+        public Attraction(String name, String workingHours, float price) {
+            this.name = name;
+            this.workingHours = workingHours;
+            this.price = price;
+        }
+    }
+
+    public static void main(String[] args) {
+        Park park = new Park();
+        park.addAttraction("Американские горки", "10:00 - 18:00", 400f);
+        park.addAttraction("Колесо обозрения", "12:00 - 21:00", 250f);
+        park.addAttraction("Бустер>", "12:00 - 20:00", 550f);
+
+        park.allAttractions();
+    }
 }
